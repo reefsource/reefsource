79ac3c5c8614e30b2649408b3f2010932e0a390e
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-08 22:41:36 -0700

    updating circle.yml

---
+++ README.md
@@ -0,0 +1,3 @@
+Reefsource
+
+[![CircleCI](https://circleci.com/gh/reefsource/reefsource.svg?style=svg)](https://circleci.com/gh/reefsource/reefsource)
--- bin/package-static.sh
+++ bin/package-static.sh
@@ -6,9 +6,3 @@ version=`python -c "import reefsource; print (reefsource.__version__)"`
 static_root=`python -c "from django.conf import settings; print (settings.STATIC_ROOT)"`
 
 aws s3 sync "${static_root}" "s3://static.coralreefsource.org/${version}/" --acl 'public-read' --cache-control 'public, max-age=31536000'
-
-# clean up previous version
-aws s3 mv "s3://static.coralreefsource.org/current" current
-aws s3 rm "s3://static.coralreefsource.org/`cat current`/" --recursive --only-show-errors
-echo ${version} > current
-aws s3 mv current "s3://static.coralreefsource.org/current"
