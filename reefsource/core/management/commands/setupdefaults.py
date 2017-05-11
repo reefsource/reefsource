4a1a1eed11087ff1cff46f3691e353c82d301cbb
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-11 14:38:51 -0700

    fixed setup defaults and setup demo

--- reefsource/core/management/commands/setupdefaults.py
+++ reefsource/core/management/commands/setupdefaults.py
@@ -1,13 +1,8 @@
 import logging
 
 from django.core.management.base import BaseCommand
-from django.db import transaction
-
-from reefsource.apps.operations.tasks import process_recurring_operations
-from reefsource.apps.securities.tasks import collect_symbol_close_prices
 
 logger = logging.getLogger(__name__)
-
 
 class Command(BaseCommand):
     help = 'Creates everything that is required for this project to work'
