1100877c6bcfa57d5ef0f1dbc07197d099ecb574
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-14 09:58:06 -0700

    cleanup

--- reefsource/apps/frontend/angular/systemjs.config.js
+++ reefsource/apps/frontend/angular/systemjs.config.js
@@ -7,7 +7,8 @@
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
-            app: 'reefsource/apps/frontend/angular',
+            'app': 'reefsource/apps/frontend/angular',
+
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -20,6 +21,7 @@
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
             '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
             '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+
             // other libraries
             'rxjs': 'npm:rxjs',
             'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
@@ -36,4 +38,3 @@
         }
     });
 })();
-//# sourceMappingURL=systemjs.config.js.map
