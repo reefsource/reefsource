7ef6a14faa36d65f1b5c29a66da900982b88b4a0
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-21 01:52:14 -0700

    use custom domain

--- reefsource/settings/docker.py
+++ reefsource/settings/docker.py
@@ -7,6 +7,7 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME', '')
 AWS_DEFAULT_ACL = 'private'
 AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', '')
 AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN', '')
+AWS_S3_CALLING_FORMAT = 'boto.s3.connection.OrdinaryCallingFormat'
 
 # proxy
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
