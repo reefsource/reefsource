aca1e1301bf8dc80cc7b96c40e1b573e436dc6e0
dailinshen <dailinshen@gmail.com>
2017-05-30 16:14:45 -0400

    Add footer.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,30 +11,77 @@ import {User} from 'app/models/user';
   selector: 'app-root',
   template: `
     <div class="container header-nav">
-    <div class="row vertical-align">
-      <h1 class="col-sm-2 text-center">REEFSOURCE</h1>
-      <nav class="col-sm-8">
-        <a routerLink="/how-it-works">How it Works</a>
-        <a routerLink="/mission">Mission</a>
-        <a routerLink="/map">Map</a>
-        <a routerLink="/about">About</a>
-        <a routerLink="/contact">Contact</a>
-          
-        <a *ngIf="!(user$ | async)" (click)="login()">Login</a>
-    
-        <a *ngIf="(user$ | async)" routerLink="/albums"> My Albums</a>
-        <span *ngIf="(user$ | async)">{{(user$ | async)?.email }} <a (click)="logout()">logout</a></span>
-      </nav>
-    </div>
+      <div class="row vertical-align">
+        <h1 class="col-sm-2 text-center">REEFSOURCE</h1>
+        <nav class="col-sm-8">
+          <a routerLink="/how-it-works">How it Works</a>
+          <a routerLink="/mission">Mission</a>
+          <a routerLink="/map">Map</a>
+          <a routerLink="/about">About</a>
+          <a routerLink="/contact">Contact</a>
+            
+          <a *ngIf="!(user$ | async)" (click)="login()">Login</a>
+      
+          <a *ngIf="(user$ | async)" routerLink="/albums"> My Albums</a>
+          <span *ngIf="(user$ | async)">{{(user$ | async)?.email }} <a (click)="logout()">logout</a></span>
+        </nav>
+      </div>
     </div>
 
     <router-outlet></router-outlet>
+    
+    
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <h4>REEFSOURCE</h4>
+            <p>A crowdsourcing environmental project funded by Brown Institute for Media Innovation.</p>
+          </div>
+          <div class="col-sm-2"></div>
+          <div class="col-sm-3 text-right">
+            <h6>CONTACT US</h6>
+            <p>Address: </p>
+            <p>Phone: </p>
+            <p>Email: </p>
+            <p></p>
+          </div>
+          <div class="col-sm-3 text-right">
+            <h6>SOCIAL MEDIA</h6>
+            <label>Medium</label>
+            <label>Twitter</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="container copyright">
+        <p class="text-center"> Copyright © REEFSOUCE 2017. All right reserved. </p>
+      </div>
+    </footer>
+    
   `,
-  styles: [`    
+  styles: [`
+    h1, h2, h3, h4, h5, a, nav, div {
+      font-family: Avenir;
+    }
+    h4, h6 {
+      color: #aed0e8;
+    }
+    p, label {
+      margin: 0 0 0 0;
+    }
     h1 {
       margin-top: auto;
       margin-bottom: auto;
       font-size: calc(14px + 0.7vw);
+    }
+    .copyright {
+      width: 100vw;
+      margin-top: 3%;
+      padding-top: 1%;
+      padding-bottom: 1%;
+      font-size: 0.9em;
+      background-color: black;
     }
     h1, a, nav {
       color: #384163 !important;
@@ -46,6 +93,11 @@ import {User} from 'app/models/user';
       position: absolute;
       transform: translateY(-50%);
       width: 100vw;
+    }
+    footer {
+      padding-top: 3%;
+      background-color: #384163;
+      color: beige;
     }
     a {
       font-size: calc(14px + 0.5vw);
