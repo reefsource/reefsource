a2b5090ce30382e9ce18f7b5c597bd69e6e5bffa
Lukasz Karolewski <lkarolewski@gmail.com>
2017-05-29 23:21:46 -0700

    fixing issue when second stage results were retriggering stage2

--- reefsource/apps/results/signals.py
+++ reefsource/apps/results/signals.py
@@ -11,6 +11,6 @@ logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=Result)
 def process_stage1_result(sender, instance: Result = None, created=False, **kwargs):
-    if created:
+    if created and instance.stage == Result.Stage.STAGE_1:
         logger.info('scheduling start of stage 2 {}'.format(instance))
         stage2.delay(instance.uploaded_file.id)
