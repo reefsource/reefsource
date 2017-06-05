a3c6ca70ed960dccc46216aff5b051d15d06ed14
Lukasz Karolewski <lkarolewski@gmail.com>
2017-06-04 23:49:51 -0700

    do not create a default album as it requires lat, lng and date

--- reefsource/apps/albums/signals.py
+++ reefsource/apps/albums/signals.py
@@ -1,20 +1,12 @@
 import logging
 
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from reefsource.apps.albums.models import Album, UploadedFile
+from reefsource.apps.albums.models import UploadedFile
 from reefsource.apps.albums.tasks import stage1
 
 logger = logging.getLogger(__name__)
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        logger.info('creating default album for {}'.format(instance))
-        Album.objects.create(user=instance, name='Default Album')
 
 
 @receiver(post_save, sender=UploadedFile)
