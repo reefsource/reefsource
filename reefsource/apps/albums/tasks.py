aaee5c0fec582e8fba2c180c8fa115c99e98d4b3
Lukasz Karolewski <lkarolewski@gmail.com>
2017-06-25 15:59:34 -0700

    trygin out rate limiting

--- reefsource/apps/albums/tasks.py
+++ reefsource/apps/albums/tasks.py
@@ -8,7 +8,7 @@ from reefsource.apps.albums.models import UploadedFile
 logger = logging.getLogger(__name__)
 
 
-@shared_task(max_retries=None)
+@shared_task(max_retries=None, rate_limit='3/m')
 def stage1(upload_id):
     try:
         UploadedFile.objects.get(pk=upload_id).start_stage1()
@@ -16,7 +16,7 @@ def stage1(upload_id):
         stage1.retry(countdown=60 * 3 + random.randint(-60, 60), exc=e)
 
 
-@shared_task(max_retries=None)
+@shared_task(max_retries=None, rate_limit='3/m')
 def stage2(upload_id):
     try:
         UploadedFile.objects.get(pk=upload_id).start_stage2()
