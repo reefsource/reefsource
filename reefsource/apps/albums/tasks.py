001de03d8fb78a981ee387b611c736245c126d6e
Lukasz Karolewski <lkarolewski@gmail.com>
2017-06-25 16:15:50 -0700

    retry with linear backoff and more logging

--- reefsource/apps/albums/tasks.py
+++ reefsource/apps/albums/tasks.py
@@ -11,14 +11,16 @@ logger = logging.getLogger(__name__)
 @shared_task(max_retries=None, rate_limit='3/m')
 def stage1(upload_id):
     try:
+        logger.info('stage 1 for {} attempt {}'.format(upload_id, stage1.request.retries))
         UploadedFile.objects.get(pk=upload_id).start_stage1()
     except Exception as e:
-        stage1.retry(countdown=60 * 3 + random.randint(-60, 60), exc=e)
+        stage1.retry(countdown=60 * stage1.request.retries + random.randint(0, 60), exc=e)
 
 
 @shared_task(max_retries=None, rate_limit='3/m')
 def stage2(upload_id):
     try:
+        logger.info('stage 2 for {} attempt {}'.format(upload_id, stage2.request.retries))
         UploadedFile.objects.get(pk=upload_id).start_stage2()
     except Exception as e:
-        stage2.retry(countdown=60 * 3 + random.randint(-60, 60), exc=e)
+        stage2.retry(countdown=60 * stage1.request.retries + random.randint(0, 60), exc=e)
