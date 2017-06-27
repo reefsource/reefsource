eb7828f5df36c91fb6bf4bec10203eab75efb4a7
Lukasz Karolewski <lkarolewski@gmail.com>
2017-06-26 23:26:19 -0700

    added score to the album details

--- client/src/app/components/how-it-works/how-it-works.component.ts
+++ client/src/app/components/how-it-works/how-it-works.component.ts
@@ -1,4 +1,9 @@
 import {Component, OnInit} from '@angular/core';
+import * as fromRoot from 'app/reducers';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+import {AuthService} from '../../services/auth.service';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-how-it-works',
@@ -6,11 +11,16 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./how-it-works.component.css']
 })
 export class HowItWorksComponent implements OnInit {
+  public user$: Observable<User>;
 
-  constructor() {
+  constructor(private store: Store<fromRoot.State>, private authService: AuthService) {
+    this.user$ = store.select(fromRoot.getUserState);
   }
 
   ngOnInit() {
   }
 
+  login() {
+     this.authService.login_google_OAuth();
+  }
 }
